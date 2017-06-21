d3b5c1cebe1418ef67ba544a460889a6e4c236de
Jacob Barnes <jmbarnes1987@gmail.com>
2017-06-21 10:46:31 -0500

    Added URL to project

--- ProsaicBinarySearch.cs
+++ ProsaicBinarySearch.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+//http://codekata.com/kata/kata02-karate-chop/
+
 namespace ProsaicBinarySearch
 {
     class KataBinarySearch
